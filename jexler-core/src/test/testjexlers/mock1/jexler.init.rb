c774b88de53a73e47e6598c2b9b0f046868396c0
jexler <jex@jexler.net>
2013-02-09 19:42:41 +0100

    whatever

--- jexler-core/src/test/testjexlers/mock1/jexler.init.rb
+++ jexler-core/src/test/testjexlers/mock1/jexler.init.rb
@@ -1,9 +1,6 @@
 import Java::net.jexler.core.MockHandler
 
-$description = "Mock Jexler 1"
+$description = "Simple mock that processes no messages"
 
-$handlers.add MockHandler.new("mock11", "Mock Jexler 1, Mock Handler 1")
-$handlers.add MockHandler.new("mock12", "Mock Jexler 1, Mock Handler 2")
-$handlers.add MockHandler.new("mock13", "Mock Jexler 1, Mock Handler 3")
-$handlers.add MockHandler.new("mock14", "Mock Jexler 1, Mock Handler 4")
-$handlers.add MockHandler.new("mock15", "Mock Jexler 1, Mock Handler 5")
+$handlers.add MockHandler.new("h1", "Mock handler 1")
+$handlers.add MockHandler.new("h2", "Mock handler 2")
