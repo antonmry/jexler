e7cb6996a387117b754132884dd1f3193846a374
jexler <jex@jexler.net>
2013-02-09 22:26:19 +0100

    whatever

--- jexler-core/src/test/java/net/jexler/core/MockHandlerTest.java
+++ jexler-core/src/test/java/net/jexler/core/MockHandlerTest.java
@@ -17,9 +17,7 @@
 package net.jexler.core;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.List;
