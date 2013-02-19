04c2f486e22bdc40057ea7ec5d704a469d12ebf9
jexler <jex@jexler.net>
2013-02-19 10:18:42 +0100

    whatever

--- jexler-war/src/main/webapp/WEB-INF/jexlers/cron.groovy
+++ jexler-war/src/main/webapp/WEB-INF/jexlers/cron.groovy
@@ -1,15 +1,15 @@
 // autostart
-import net.jexler.StopService;
-import net.jexler.CronService;
+import net.jexler.StopService
+import net.jexler.CronService
 
-services.add(new CronService(jexler, "every-minute").setCron("* * * * *").start());
-services.add(new CronService(jexler, "every-two-minutes").setCron("*/2 * * * *").start());
+services.add(new CronService(jexler, "every-minute").setCron("* * * * *").start())
+services.add(new CronService(jexler, "every-two-minutes").setCron("*/2 * * * *").start())
 
 while (true) {
   event = events.take();
   if (event instanceof CronService.Event) {
-    println ("It is now (groovy): " + new Date() + " (" + event.getServiceId() + ")");
+    println ("It is now (groovy): " + new Date() + " (" + event.getServiceId() + ")")
   } else if (event instanceof StopService.Event) {
-    return;
+    return
   }
 }
--- jexler-war/src/main/webapp/WEB-INF/jexlers/filetailer.groovy
+++ jexler-war/src/main/webapp/WEB-INF/jexlers/filetailer.groovy
@@ -1,18 +1,18 @@
 // autostart
-import net.jexler.StopService;
-import net.jexler.FileTailerService;
+import net.jexler.StopService
+import net.jexler.FileTailerService
 
-s = new FileTailerService(jexler, "selftailer");
-s.setFile(file.getAbsolutePath());
-s.addFilterPattern("^import");
-s.start();
-services.add(s);
+s = new FileTailerService(jexler, "selftailer")
+s.setFile(file.getAbsolutePath())
+s.addFilterPattern("^import")
+s.start()
+services.add(s)
 
 while (true) {
   event = events.take();
   if (event instanceof FileTailerService.Event) {
-    println ("Got line (groovy): " + event.getLine());
+    println ("Got line (groovy): " + event.getLine())
   } else if (event instanceof StopService.Event) {
-    return;
+    return
   }
 }
