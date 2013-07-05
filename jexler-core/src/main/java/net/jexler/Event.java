6c06a9af54b1748517a0c5a570e56d9a311e7cd7
jexler <jex@jexler.net>
2013-07-05 18:08:27 +0200

    whatever

---
+++ jexler-core/src/main/java/net/jexler/Event.java
@@ -0,0 +1,35 @@
+/*
+   Copyright 2012-now $(whois jexler.net)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package net.jexler;
+
+import net.jexler.service.Service;
+
+
+/**
+ * Interface for an event (sent by a service to a jexler).
+ *
+ * @author $(whois jexler.net)
+ */
+public interface Event {
+
+    /**
+     * Get service that sent the event.
+     * @return sensor
+     */
+    Service getService();
+
+}
