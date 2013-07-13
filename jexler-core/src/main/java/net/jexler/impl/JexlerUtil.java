d0c8ae4f3a90aa01df90603ce61527ace8430850
jexler <jex@jexler.net>
2013-07-13 07:49:32 +0200

    whatever

--- jexler-core/src/main/java/net/jexler/impl/JexlerUtil.java
+++ jexler-core/src/main/java/net/jexler/impl/JexlerUtil.java
@@ -58,9 +58,9 @@ public class JexlerUtil {
                     break;
                 }
                 if (reverseLines) {
-                    builder.insert(0, line + "\n");
+                    builder.insert(0, line + System.lineSeparator());
                 } else {
-                    builder.append(line + "\n");
+                    builder.append(line + System.lineSeparator());
                 }
             } while (true);
         }
