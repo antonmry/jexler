e70c3ca85a161c4302ac618f19373456b770ae9b
jexler <jex@jexler.net>
2015-05-10 22:54:20 +0200

    back

--- jexler/src/main/java/net/jexler/war/JexlerContextListener.java
+++ jexler/src/main/java/net/jexler/war/JexlerContextListener.java
@@ -45,7 +45,7 @@ public class JexlerContextListener implements ServletContextListener    {
 
 	static final Logger log = LoggerFactory.getLogger(JexlerContextListener.class);
 
-    private static final String guiVersion = "2.0.0-SNAPSHOT"; // IMPORTANT: keep in sync with version in main build.gradle
+    private static final String guiVersion = "1.0.16-SNAPSHOT"; // IMPORTANT: keep in sync with version in main build.gradle
 	
 	private static String version;
     private static ServletContext servletContext;
